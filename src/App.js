bc2fc18b779cd65a5daee5c708a921bff403ded6
Hideki Nara <hdknr@shiftone.ai>
2025-04-27 17:11:10 +0900

    PanoramaViewer 組み込み

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import PanoramaViewer from './PanoramaViewer.js'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div>
+        <PanoramaViewer imageUrl="assets/panoramas/panorama01.jpg"></PanoramaViewer>
+      </div>
+
     </div>
   );
 }
